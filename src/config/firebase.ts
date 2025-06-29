bc2e04d4862b8692032a67bb253b515f9a4d6e42
Hari <hari@Haris-MacBook-Air.local>
2025-06-29 14:18:01 +0800

    init project wit firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4bQuh9Y2fDBce0n3zGXHehTpRiGPp91I",
+  authDomain: "plastation136.firebaseapp.com",
+  projectId: "plastation136",
+  storageBucket: "plastation136.firebasestorage.app",
+  messagingSenderId: "807118955466",
+  appId: "1:807118955466:web:6c4a8a063deaef57df2533",
+  measurementId: "G-PZ1JC0W987",
+};
+
+export const app = initializeApp(firebaseConfig);
